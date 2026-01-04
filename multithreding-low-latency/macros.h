aa7d3cf51684baa280c92468b5498c02c5b63cf8
Piotr Kowalski <piotrkow93@gmail.com>
2026-01-04 16:36:32 +0100

    Multithreading utils, core affinity

--- multithreding-low-latency/macros.h
+++ multithreding-low-latency/macros.h
@@ -10,14 +10,14 @@
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
 // in C++20 [[likely]] [[unlikely]]
 
-inline auto ASSERT(bool cond, const std::string& msg) noexcept {
+inline auto ASSERT(bool cond, const std::string &msg) noexcept {
     if(UNLIKELY(!cond)){
         std::cerr << msg << std::endl;
         exit(EXIT_FAILURE);
     }
 }
 
-inline auto FATAL(const std::string& msg) noexcept {
+inline auto FATAL(const std::string &msg) noexcept {
     std::cerr << msg << std::endl;
     exit(EXIT_FAILURE);
 }
---
+++ multithreding-low-latency/multithreding.cpp
@@ -0,0 +1,29 @@
+﻿#include "macros.h"
+#include "thread_utils.h"
+
+auto dummyFunction(int a, int b, bool sleep) {
+	std::cout << "dummyFunction(" << a << "," << b << ")" << std::endl;
+	std:: cout << "dummyFunction output = " << a + b << std::endl;
+
+	if(sleep){
+		using namespace std::literals::chrono_literals;
+        std::this_thread::sleep_for(5s);
+	}
+
+	std::cout << "dummyFunction done." << std::endl;
+}
+
+int main()
+{
+	auto t1 = createAndStartThread(-1, "dummyFunc1", dummyFunction, 12, 21, false);
+	auto t2 = createAndStartThread(1, "dummyFunc2", dummyFunction, 15, 51, true);
+
+	std::cout << "Waiting for threads..." << std::endl;
+
+	t1->join();
+	t2->join();
+
+	std::cout << "main exiting" << std::endl;
+
+	return 0;
+}
