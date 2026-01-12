43e909dd096fb4b976781f07e3909e9501b67fd3
Piotr Kowalski <piotrkow93@gmail.com>
2026-01-12 23:38:07 +0100

    First implementation of binary logger

--- binary-logger-ring-buffer/binaryLogger.h
+++ binary-logger-ring-buffer/binaryLogger.h
@@ -1,16 +1,82 @@
 #pragma once
 
-#include <iostream>
+// ????
+#include <fcntl.h>
+// ????
+#include <unistd.h>
+// ????
+#include <sys/mman.h>
+#include <sys/stat.h>
 
+#include <cstdint>
+#include <cstring>
+#include <iostream>
 
 class BinaryLogger final {
     private:
-        std::string file_name_;
-
+        size_t      file_size_;
+        size_t      write_pos_;
+        int         fd_;        // File descriptor
+        uint8_t*    mmap_ptr_;  // Memory poiner
 
     public:
-        auto log(std::string &str) noexcept {
-            //TODO: use mmap and memcpy
+        explicit BinaryLogger(const char* file) :
+            file_size_(1024*1024), // 1MB log file
+            mmap_ptr_(nullptr),
+            fd_(-1),
+            write_pos_(0)
+        {
+            // open() returns int 
+            // Each linux process contains list of open files (0,1,2,3....)
+            // fd - are indexes in this list, so this int means "number that identifies my opened file"
+            // O_RDWR - I want to read and write to this file
+            // O_CREAT - if file does not exist, create it
+            // 0644 - UNIX access 0, 6 - owner (write+read), 4 - group (read), 4 - others (read)
+            int fd = open(file, O_RDWR | O_CREAT, 0644);
+
+            // Set up file size (does not work on 0bytes)
+            // gives 1MB to the file (it is empty logically)
+            if (ftruncate(fd_, file_size_) != 0) {
+                close(fd_);
+            }
+
+            // We need to add desciptor - "Do memory maping for the file under this descriptor"
+            // PROT_READ | PROT_WRITE i need to read and write to this memory
+            // MAP_SHARED ???
+            void* addr = mmap(nullptr, file_size_, PROT_READ | PROT_WRITE, MAP_SHARED, fd_, 0);
+
+            if (addr == MAP_FAILED) {
+                // After closing fd_, mapping stil works
+                // mmap and fd has two different lifecycles
+                close(fd_);
+            }
+
+            // Map to bytes
+            // mmap if succeded return addres, we need to cast it
+            // to be able to do poiner arythemic 
+            mmap_ptr_ = static_cast<uint8_t*>(addr);
         }
 
+        ~BinaryLogger() {
+            if (mmap_ptr_) {
+                // ????
+                munmap(mmap_ptr_, file_size_);
+            }
+
+            if (fd_ >= 0) {
+                close(fd_);
+            }
+        }
+
+        auto log(std::string &str) noexcept {
+            const uint32_t len = static_cast<uint32_t>(str.size());
+
+            // Save lenght
+            std::memcpy(mmap_ptr_ + write_pos_, &len, sizeof(len));
+            write_pos_ += sizeof(len);
+
+            // Save data
+            std::memcpy(mmap_ptr_ + write_pos_, str.data(), len);
+            write_pos_ += len;
+        }
 };
